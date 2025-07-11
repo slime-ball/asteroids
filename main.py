42c36e30e5b464e819d726118284062f20c31232
slime-ball <echan63@binghamton.edu>
2025-07-11 14:42:58 -0400

    Finished the Player code

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -13,9 +17,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)  
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)  
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
--- player.py
+++ player.py
@@ -28,7 +28,16 @@ class Player(CircleShape):
 
         if keys[pygame.K_d]:
             rotation = self.rotate(dt)
+        
+        if keys[pygame.K_w]:
+            forward = self.move(dt)
+
+        if keys[pygame.K_s]:
+            forward = self.move(dt)
 
     def rotate(self, dt):
-        self.rotation = PLAYER_TURN_SPEED * dt
+        self.rotation += PLAYER_TURN_SPEED * dt
         
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
